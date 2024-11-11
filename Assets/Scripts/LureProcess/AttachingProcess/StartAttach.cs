81a627b1e8972c8dc80aeec51e9a545487a35641
HP <samu.kauppi@metropolia.fi>
2024-11-11 10:21:31 +0200

    add terrain to scene

--- Assets/Scripts/LureProcess/AttachingProcess/StartAttach.cs
+++ Assets/Scripts/LureProcess/AttachingProcess/StartAttach.cs
@@ -6,6 +6,25 @@ using UnityEngine;
 public class StartAttach : MonoBehaviour
 {
     [SerializeField] private AttachingType objectToAttach;
+    private Outline outline;
+    private void Start()
+    {
+        if (TryGetComponent<Outline>(out var line))
+        {
+            outline = line;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        if (outline != null)
+            outline.enabled = true;
+    }
+    private void OnMouseExit()
+    {
+        if (outline != null)
+            outline.enabled = false;
+    }
 
     private void OnMouseDown()
     {
