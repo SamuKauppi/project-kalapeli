f02b7b2c7d79357ac2bbb369299e1b1a56d61917
HP <samu.kauppi@metropolia.fi>
2024-11-21 10:34:54 +0200

    remove mouse rotation because it was buggy

--- Assets/Scripts/LureProcess/LureProperties/BlockRotation.cs
+++ Assets/Scripts/LureProcess/LureProperties/BlockRotation.cs
@@ -23,10 +23,6 @@ public class BlockRotation : MonoBehaviour
     private int sideRotIndex = 0;   // Which side rotation is used
     private int upRotIndex = 0;     // Which up rotation is used
 
-    [SerializeField] private float mouseRotationSpeed = 10f;
-    private bool isRotatingByMouse; // Is the block rotated by mouse
-    private Vector3 mousePos;
-    private Vector3 lastMousePos;
 
     // Used to avoid restart same up-rotation corountine
     private enum UpRotation
@@ -49,19 +45,7 @@ public class BlockRotation : MonoBehaviour
     {
         if (StopRotating) { return; }
 
-        if (isRotatingByMouse && Input.GetMouseButtonUp(1))
-        {
-            isRotatingByMouse = false;
-            IsRotating = false;
-            StartCoroutine(RotateTransform(0, 0, 0.1f));
-        }
-
-        RotateByMouse();
-
-        if (!isRotatingByMouse)
-        {
-            RotateByKeys();
-        }
+        RotateByKeys();
     }
 
     private void RotateByKeys()
@@ -95,25 +79,6 @@ public class BlockRotation : MonoBehaviour
             if (IsRotating) StopAllCoroutines();
             StartCoroutine(RotateTransform(1 * (upRotIndex == 0 ? 1 : upRotIndex), 0, rotationTime));
         }
-    }
-
-    private void RotateByMouse()
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            isRotatingByMouse = true;
-            lastMousePos = Input.mousePosition;
-            IsRotating = true;
-        }
-
-        if (isRotatingByMouse)
-        {
-            mousePos = Input.mousePosition;
-            Vector3 deltaPos = mousePos - lastMousePos;
-            transform.Rotate(-deltaPos.y * mouseRotationSpeed, deltaPos.x * mouseRotationSpeed, 0f, Space.Self);
-            lastMousePos = mousePos;
-        }
-
     }
 
     /// <summary>
